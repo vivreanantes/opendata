b31397fae272af291c98821d37ce5388e7bd26bf
Christian Renoulin <chris-ren@netcourrier.com>
2014-01-29 20:30:32 +0100

    début packaging pour version release

--- build/MieuxTrierANantes_ko/assets/app/view/garbages/InformationsList.js
+++
@@ -1,5 +0,0 @@
-Ext.define('VivreANantes.view.garbages.InformationsList', {
-	extend : 'Ext.List',
-	xtype : 'informationsList_xtype'
-});
-
--- build/MieuxTrierANantes_ok/assets/app/view/garbages/UsualCategoriesList2.js
+++
@@ -1,9 +0,0 @@
-/**
- * Catégories usuelles : page avant boutons
- * 
- * @author Christian Renoulin
- */
-Ext.define('VivreANantes.view.garbages.UsualCategoriesList2', {
-	extend : 'Ext.List',
-			xtype : 'usualCategoriesList2_xtype'
-});
